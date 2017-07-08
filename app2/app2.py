9da94580f912ec473b1cc2727c6fd2d9028764cf
ThejanW <thejanwijesinghe.14@cse.mrt.ac.lk>
2017-07-08 17:16:12 +0530

    Initial commit

---
+++ app2/app2.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import flask
+
+port = "8764"
+
+app = flask.Flask(__name__)
+
+
+@app.route("/")
+def index():
+    """The index page which provide information about other API end points"""
+
+    return "I am app 2"
+
+
+if __name__ == '__main__':
+    print("Serving on port %s" % port)
+    app.run(host='0.0.0.0', port=port)
